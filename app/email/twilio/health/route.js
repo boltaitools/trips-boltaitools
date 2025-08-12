2f8b672bfdedead26b018504362577ca43b62a18
boltaitools <blue@boltaitools.com>
2025-08-12 16:59:02 -0400

    Build OK locally – deploy

--- app/email/twilio/health/route.js
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  return new Response(JSON.stringify({ ok: true, message: "Twilio health is good" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" }
-  });
-}
